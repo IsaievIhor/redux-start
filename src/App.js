4df023db87a79da769f55eab7d64f743801b5d08
Isaev Igor <igor.igorovich.isaev@gmail.com>
2021-09-08 14:54:55 +0300

    useselector usedispatch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
 import React from "react";
+import {useDispatch, useSelector} from "react-redux";
 
 function App() {
-  return (
-    <div className="app">
-qwerty
-    </div>
-  );
+    const dispatch = useDispatch()
+    const cash = useSelector(state => state.cash)
+    console.log(cash)
+
+    const addCash = (cash) => {
+        dispatch({type:'ADD_CASH', payload: cash})
+    }
+
+    const getCash = (cash) => {
+        dispatch({type:'GET_CASH', payload: cash})
+    }
+
+    return (
+        <div className="app">
+            <div style={{fontSize: '30px'}}>{cash}</div>
+            <div style={{display: 'flex'}}>
+                <button onClick={() => addCash(Number(prompt()))}>Пополнить счет</button>
+                <button onClick={() => getCash(Number(prompt()))}>Снять со счета</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
